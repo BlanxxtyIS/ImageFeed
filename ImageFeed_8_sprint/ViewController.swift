19d3f9dafb09ab24396890e4d4b4ce15009a67af
Blanxxty <Mvtvr21@yandex.ru>
2023-08-30 16:14:11 +0300

    Initial Commit

---
+++ ImageFeed_8_sprint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed_8_sprint
+//
+//  Created by Марат Хасанов on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
