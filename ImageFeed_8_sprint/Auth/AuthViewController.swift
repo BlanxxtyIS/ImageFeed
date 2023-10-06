670ea4aaba1f5d7c38b7d1626bc6f4e4c82ec420
Blanxxty <Mvtvr21@yandex.ru>
2023-10-06 08:54:52 +0300

    10 sprint start

---
+++ ImageFeed_8_sprint/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed_8_sprint
+//
+//  Created by Марат Хасанов on 06.10.2023.
+//
+
+import Foundation
+
+class AuthViewController: UIViewController {
+    
+}
